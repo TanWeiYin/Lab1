82437fe0cec33d256859b48ebf40a71a20d74f8d
Emaliana Kasmuri <emaliana.kasmuri@gmail.com>
2021-03-18 21:53:53 +0800

    initial comit for location.java

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,18 @@
+package samples;
+
+public class Location 
+{
+	private String words; // private = restricted access
+
+	  // Getter
+	  public String getName() 
+	  {
+	    return words;
+	  }
+
+	  // Setter
+	  public void setName(String newWords) 
+	  {
+	    this.words = newWords;
+	  }
+}
